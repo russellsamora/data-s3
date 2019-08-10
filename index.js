8a201443590063737b253dfd9a71500df25ce69a
russellgoldenberg <russellgoldenberg@gmail.com>
2019-08-10 16:08:27 -0400

    the big three

--- index.js
+++ index.js
@@ -2,113 +2,146 @@ const AWS = require('aws-sdk');
 const dsv = require('d3-dsv');
 
 const s3 = new AWS.S3();
+let configured = false;
 
 // private
 function getFormat(file) {
-	return new Promise((resolve, reject) => {
-		const supported = ['csv', 'tsv', 'json'];
-		try {
-			const s = file.split('.');
-			if (s.length) {
-				const f = s.pop();
-				if (supported.includes(f)) resolve(f);
-				else reject(`unsupported format: ${f}`);
-			} else reject('no extension in filename');
-		} catch (err) {
-			reject(`issue parsing format from ${file}`);
+	const supported = ['csv', 'tsv', 'json'];
+	try {
+		const s = file.split('.');
+		if (s.length) {
+			const f = s.pop();
+			if (supported.includes(f)) return Promise.resolve(f);
+			return Promise.reject(`unsupported format: ${f}`);
 		}
-	});
+		return Promise.reject('no extension in filename');
+	} catch (err) {
+		return Promise.reject(err.message);
+	}
 }
 
 function stringify({ data, format }) {
-	return new Promise((resolve, reject) => {
-		try {
-			let output = null;
-			if (format === 'csv') output = dsv.csvFormat(data);
-			if (format === 'tsv') output = dsv.tsvFormat(data);
-			if (format === 'json') output = JSON.stringify(data);
-			resolve(output);
-		} catch (err) {
-			reject(`issue stringifying data as a ${format}`);
-		}
-	});
+	try {
+		let output = null;
+		if (format === 'csv') output = dsv.csvFormat(data);
+		if (format === 'tsv') output = dsv.tsvFormat(data);
+		if (format === 'json') output = JSON.stringify(data);
+		return Promise.resolve(output);
+	} catch (err) {
+		return Promise.reject(`issue stringifying data as a ${format}`);
+	}
 }
 
 function parse({ buffer, format }) {
+	try {
+		const str = buffer.toString('utf8');
+		let output = null;
+		if (format === 'csv') output = dsv.csvParse(str);
+		if (format === 'tsv') output = dsv.tsvParse(str);
+		if (format === 'json') output = JSON.parse(str);
+		return Promise.resolve(output);
+	} catch (err) {
+		return Promise.reject(`issue parsing data as a ${format}`);
+	}
+}
+
+function uploadS3(params) {
 	return new Promise((resolve, reject) => {
-		try {
-			const str = buffer.toString('utf8');
-			let output = null;
-			if (format === 'csv') output = dsv.csvParse(str);
-			if (format === 'tsv') output = dsv.tsvParse(str);
-			if (format === 'json') output = JSON.parse(str);
-			resolve(output);
-		} catch (err) {
-			reject(`issue parsing data as a ${format}`);
-		}
+		s3.upload(params, (err, data) => {
+			if (err) reject(`${err.statusCode} ${err.message}`);
+			else if (data) resolve('success');
+			else reject('no data');
+		});
+	});
+}
+
+function downloadS3(params) {
+	return new Promise((resolve, reject) => {
+		s3.getObject(params, async (err, resp) => {
+			if (err) reject(`${err.statusCode} ${err.message}`);
+			else if (resp) {
+				try {
+					const format = await getFormat(params.Key);
+					const buffer = resp.Body;
+					const data = await parse({ buffer, format });
+					resolve(data);
+				} catch (err) {
+					reject(err);
+				}
+			} else reject('no response');
+		});
+	});
+}
+
+function existsS3(params) {
+	return new Promise((resolve, reject) => {
+		s3.headObject(params, (err, resp) => {
+			if (err && err.statusCode === 404) resolve(false);
+			else if (resp) resolve(true);
+			else reject('error fetching file');
+		});
 	});
 }
 
 // public
 async function upload({ bucket, file, data }) {
-	return new Promise (async (resolve, reject) => {
-		if (!bucket || !file || !data) reject('missing parameters');
-		else {
-			try {
-				const format = await getFormat(file);
-				const body = await stringify({ data, format });
-				
-				const params = {
-					Bucket: bucket,
-					Body: body,
-					Key: file
-				};
+	if (!configured) return Promise.reject('data-s3 not intialized');
+	if (!bucket || !file || !data) return Promise.reject('missing parameters');
 
-				s3.upload(params, (err, data) => {
-					if (err) reject(`${err.statusCode} ${err.message}`);
-					else if (data) resolve(data);
-					else reject('no data');
-				});
-			} catch(err) {
-				reject(err);
-			}	
-		}
-	});
+	try {
+		const format = await getFormat(file);
+		const body = await stringify({ data, format });
+
+		const params = {
+			Bucket: bucket,
+			Body: body,
+			Key: file
+		};
+
+		const result = await uploadS3(params);
+		return Promise.resolve(result);
+	} catch (err) {
+		return Promise.reject(err);
+	}
 }
 
-function download({ bucket, file }) {
-	return new Promise(async (resolve, reject) => {
-		if (!bucket || !file) reject('missing parameters');
-		else {
-			try {
-				const format = await getFormat(file);
+async function download({ bucket, file }) {
+	if (!configured) return Promise.reject('data-s3 not intialized');
+	if (!bucket || !file) return Promise.reject('missing parameters');
 
-				const params = {
-					Bucket: bucket,
-					Key: file
-				};
+	try {	
+		const params = {
+			Bucket: bucket,
+			Key: file
+		};
 
-				s3.getObject(params, async (err, resp) => {
-					if (err) reject(`${err.statusCode} ${err.message}`);
-					else if (resp) {
-						const buffer = resp.Body;
-						const data = await parse({ buffer, format });
-						resolve(data);
-					} else reject('no response');
-				});
-			} catch (err) {
-				reject(err);
-			}
-		}
-	});
+		const result = await downloadS3(params);
+		return Promise.resolve(result);
+	} catch (err) {
+		return Promise.reject(err);
+	}
 }
 
-function exists() {
+async function exists({ bucket, file }) {
+	try {
+		if (!configured) return Promise.reject('data-s3 not intialized');
+		if (!bucket || !file) return Promise.reject('missing parameters');
 
+		const params = {
+			Bucket: bucket,
+			Key: file
+		};
+
+		const e = await existsS3(params);
+		return Promise.resolve(e);
+	} catch (err) {
+		return Promise.reject(err);
+	}
 }
 
 function init({ accessKeyId, secretAccessKey }) {
 	AWS.config.update({ accessKeyId, secretAccessKey });
+	configured = true;
 }
 
 module.exports = { init, upload, download, exists };
